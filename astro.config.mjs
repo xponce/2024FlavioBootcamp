2228f7e321c534e2f40f81e48c90027752e4c1ea
Xavi <xaviergponce@gmail.com>
2024-03-17 22:18:20 -0700

    Added Close the Modal outside of it

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
+import 'dotenv/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
